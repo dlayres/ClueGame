cca119abf50fa621b5cb4aea38f2e37e67ff71e1
David Ayres <dlayres@mymail.mines.edu>
2018-03-03 12:00:20 -0700

    Made clueGame package

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,24 @@
+/**
+ * @author Johnathan Castillo
+ * @author David Ayres
+ * C12A-1 Clue Paths
+ */
+package clueGame;
+
+public class BoardCell {
+	private int row;
+	private int col;
+	
+	BoardCell (int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "row=" + row + ", col=" + col;
+	}
+	
+	
+}
