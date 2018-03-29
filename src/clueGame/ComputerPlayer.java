f28c64235687edd136225d3236b35e369c406be8
David Ayres <dlayres@mymail.mines.edu>
2018-03-29 13:17:32 -0600

    Started tracking ComputerPlayer and HumanPlayer class files

---
+++ src/clueGame/ComputerPlayer.java
@@ -0,0 +1,9 @@
+package clueGame;
+
+public class ComputerPlayer extends Player {
+
+	public ComputerPlayer(int row, int column, String playerName, String color) {
+		super(row, column, playerName, color);
+	}
+
+}
---
+++ src/clueGame/HumanPlayer.java
@@ -0,0 +1,13 @@
+package clueGame;
+
+public class HumanPlayer extends Player{
+	
+	public HumanPlayer() {
+		super();
+	}
+
+	public HumanPlayer(int row, int column, String playerName, String color) {
+		super(row, column, playerName, color);
+	}
+	
+}
