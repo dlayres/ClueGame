d0baaba3cacd90b6cafd55e20e0716083fa9a780
Johnathan Castillo <jkcastillo@mymail.mines.edu>
2018-04-05 11:19:49 -0600

    Added gameActionTests class

---
+++ src/tests/gameActionTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class gameActionTests {
+
+	@BeforeClass
+	public static void setUp() {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
