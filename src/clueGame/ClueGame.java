253ce84fb752e04fe660f7e2f31a629ec0e77693
David Ayres <dlayres@mymail.mines.edu>
2018-04-14 10:51:37 -0600

    Added combo boxes for players, rooms, and weapons guesses

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -22,7 +22,7 @@ import javax.swing.border.TitledBorder;
 public class ClueGame extends JFrame {
 
 	private static Board board; // Board GUI JPanel
-	private JFrame detectiveNotesGUI;
+	private DetectiveNotes detectiveNotes;
 
 	public ClueGame() {
 		// Board is singleton, get the only instance there is
@@ -56,7 +56,8 @@ public class ClueGame extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		menuBar.add(createFileMenu());
-		initializeDetectiveNotes(); // Creates detective notes GUI
+		
+		detectiveNotes = new DetectiveNotes();
 	}
 	
 	/**
@@ -78,7 +79,7 @@ public class ClueGame extends JFrame {
 		JMenuItem item = new JMenuItem("Detective Notes");
 		class MenuItemListener implements ActionListener{
 			public void actionPerformed(ActionEvent e){ // Listens for click
-				detectiveNotesGUI.setVisible(true); // Displays GUI when clicked
+				detectiveNotes.setVisible(true);
 			}
 		}
 		item.addActionListener(new MenuItemListener());
@@ -99,104 +100,7 @@ public class ClueGame extends JFrame {
 		item.addActionListener(new MenuItemListener());
 		return item;
 	}
-	
-	/**
-	 * Creates the detective notes GUI with checklists and combo boxes corresponding to players, rooms, and weapons
-	 */
-	public void initializeDetectiveNotes(){
-		// Create the basic JFrame for the detective notes GUI
-		detectiveNotesGUI = new JFrame();
-		detectiveNotesGUI.setTitle("Detective Notes");
-		detectiveNotesGUI.setSize(500, 575);
-		detectiveNotesGUI.setLayout(new GridLayout(3, 1));
-		
-		// Add a panel for the people in the game
-		JPanel peoplePanel = new JPanel(); 
-		peoplePanel.setLayout(new GridLayout(1,2)); // Left column for people checklist, right column for people combo box
-		JPanel peopleChecklistPanel = new JPanel();
-		peopleChecklistPanel.setLayout(new GridLayout(1, 2)); // Checklist panel has two columns to display all the people
-		peopleChecklistPanel.setBorder(new TitledBorder (new EtchedBorder(), "People"));
-		JPanel peopleChecklistLeft = new JPanel(); // Left checklist column
-		JPanel peopleChecklistRight = new JPanel(); // Right checklist column
-		peopleChecklistPanel.add(peopleChecklistLeft); // Add both to the checklist panel
-		peopleChecklistPanel.add(peopleChecklistRight); 
-		Set<Card> playerCardSet = board.getPlayerCards(); // Use player cards to get player names
-		int i = 0; // Determines if check box should be in the left or right column
-		for(Card nextPlayerCard : playerCardSet){ // For all player cards
-			JCheckBox personCheckBox = new JCheckBox(nextPlayerCard.getCardName()); // Make a check box with that name
-			personCheckBox.setSelected(false);
-			if(i < (Math.ceil(playerCardSet.size() / 2.0))) { // Put half on the left and half on the right
-				peopleChecklistLeft.add(personCheckBox);
-			}
-			else {
-				peopleChecklistRight.add(personCheckBox);
-			}
-			i++;
-		}
-		JPanel peopleGuessPanel = new JPanel();
-		peopleGuessPanel.setBorder(new TitledBorder (new EtchedBorder(), "Person Guess"));
-		peoplePanel.add(peopleChecklistPanel); // Add people checklist panel to the people panel
-		peoplePanel.add(peopleGuessPanel);
-		detectiveNotesGUI.add(peoplePanel); // Add people panel to the detective notes JFrame
-		
-		// Add a panel for the rooms in the game
-		JPanel roomPanel = new JPanel(); 
-		roomPanel.setLayout(new GridLayout(1,2)); // Left column for rooms checklist, right column for rooms combo box
-		JPanel roomChecklistPanel = new JPanel();
-		roomChecklistPanel.setLayout(new GridLayout(1, 2)); // Checklist panel has two columns to display all the rooms
-		roomChecklistPanel.setBorder(new TitledBorder (new EtchedBorder(), "Rooms"));
-		JPanel roomChecklistLeft = new JPanel(); // Left checklist column
-		JPanel roomChecklistRight = new JPanel(); // Right checklist column
-		roomChecklistPanel.add(roomChecklistLeft); // Add both to the checklist panel
-		roomChecklistPanel.add(roomChecklistRight);
-		Set<Card> roomCardSet = board.getRoomCards(); // Use room cards to get room names
-		i = 0; // Determines if check box should be in the left or right column
-		for(Card nextRoomCard : roomCardSet){ // For all room cards
-			JCheckBox roomCheckBox = new JCheckBox(nextRoomCard.getCardName()); // Make a check box with that name
-			roomCheckBox.setSelected(false);
-			if(i < (Math.ceil(roomCardSet.size() / 2.0))) { // Put half on the left and half on the right
-				roomChecklistLeft.add(roomCheckBox);
-			}
-			else {
-				roomChecklistRight.add(roomCheckBox);
-			}
-			i++;
-		}
-		JPanel roomGuessPanel = new JPanel();
-		roomGuessPanel.setBorder(new TitledBorder (new EtchedBorder(), "Room Guess"));
-		roomPanel.add(roomChecklistPanel); // Add rooms checklist panel to the rooms panel
-		roomPanel.add(roomGuessPanel);
-		detectiveNotesGUI.add(roomPanel); // Add rooms panel to the detective notes JFrame
-		
-		// Add a panel for the weapons in the game
-		JPanel weaponPanel = new JPanel(); 
-		weaponPanel.setLayout(new GridLayout(1,2)); // Left column for weapons checklist, right column for weapons combo box
-		JPanel weaponChecklistPanel = new JPanel();
-		weaponChecklistPanel.setLayout(new GridLayout(1, 2)); // Checklist panel has two columns to display all the weapons
-		weaponChecklistPanel.setBorder(new TitledBorder (new EtchedBorder(), "Weapons")); 
-		JPanel weaponChecklistLeft = new JPanel(); // Left checklist column
-		JPanel weaponChecklistRight = new JPanel(); // Right checklist column
-		weaponChecklistPanel.add(weaponChecklistLeft); // Add both to the checklist panel
-		weaponChecklistPanel.add(weaponChecklistRight);
-		Set<Card> weaponCardSet = board.getWeaponCards(); // Use weapon cards to get weapon names
-		i = 0; // Determines if check box should be in the left or right column
-		for(Card nextWeaponCard : weaponCardSet){ // For all weapon cards
-			JCheckBox weaponCheckBox = new JCheckBox(nextWeaponCard.getCardName()); // Make a check box with that name
-			weaponCheckBox.setSelected(false);
-			if(i < (Math.ceil(weaponCardSet.size() / 2.0))) { // Put half on the left and half on the right
-				weaponChecklistLeft.add(weaponCheckBox);
-			}
-			else {
-				weaponChecklistRight.add(weaponCheckBox);
-			}
-			i++;
-		}
-		JPanel weaponGuessPanel = new JPanel();
-		weaponGuessPanel.setBorder(new TitledBorder (new EtchedBorder(), "Weapon Guess"));
-		weaponPanel.add(weaponChecklistPanel); // Add weapons checklist to the weapons panel
-		weaponPanel.add(weaponGuessPanel);
-		detectiveNotesGUI.add(weaponPanel); // Add weapons panel to the detective notes JFrame
-	}
+
 
 	public static void main(String[] args) {
 		ClueGame game = new ClueGame(); // Make ClueGame GUI
