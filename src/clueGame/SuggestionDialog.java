d9faa0ca696b7cc39923fd1d5853bd6fbb7556fd
David Ayres <dlayres@mymail.mines.edu>
2018-04-19 14:31:04 -0600

    Started tracking new SuggestionDialog class

---
+++ src/clueGame/SuggestionDialog.java
@@ -0,0 +1,94 @@
+package clueGame;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class SuggestionDialog extends JDialog{
+
+	private static Board board;
+	private JLabel roomChoiceLabel;
+	private JComboBox playerChoiceBox;
+	private JComboBox weaponChoiceBox;
+	
+	public SuggestionDialog(){
+		
+		board = board.getInstance();
+
+		setTitle("Make a suggestion");
+		setSize(300, 250);
+		setLayout(new BorderLayout());
+		JPanel suggestionButtons = new JPanel();
+		suggestionButtons.setLayout(new GridLayout(1, 2));
+		JButton suggestionSubmitButton = new JButton("Submit");
+		suggestionSubmitButton.addActionListener(new ActionListener(){ // Listens for a button click
+			public void actionPerformed(ActionEvent e) {
+				Solution newSuggestion = new Solution((String)playerChoiceBox.getSelectedItem(), roomChoiceLabel.getText(), (String)weaponChoiceBox.getSelectedItem());
+				System.out.println(newSuggestion.player + " " + newSuggestion.room + " " + newSuggestion.weapon);
+				
+				board.latestAnswer = newSuggestion;
+				
+				
+				
+				//board.handleSuggestion(0, newSuggestion, board.getPlayerList());
+			}
+		});
+		JButton suggestionCancelButton = new JButton("Cancel");
+		suggestionCancelButton.addActionListener(new ActionListener(){ // Listens for a button click
+			public void actionPerformed(ActionEvent e) {
+				dispose(); // Closes the suggestion dialog when "Cancel" is clicked
+			}
+		});
+		suggestionButtons.add(suggestionSubmitButton);
+		suggestionButtons.add(suggestionCancelButton);
+		add(suggestionButtons, BorderLayout.SOUTH);
+
+		JPanel suggestionChoices = new JPanel();
+		suggestionChoices.setLayout(new GridLayout(3, 1));
+		JPanel playerChoice = new JPanel();
+		playerChoice.setLayout(new GridLayout(1, 2));
+		JPanel roomChoice = new JPanel();
+		roomChoice.setLayout(new GridLayout(1, 2));
+		JPanel weaponChoice = new JPanel();
+		weaponChoice.setLayout(new GridLayout(1, 2));
+
+		JLabel roomSuggestionLabel = new JLabel("Current room");
+		JLabel playerSuggestionLabel = new JLabel("Player choice");
+		JLabel weaponSuggestionLabel = new JLabel("Weapon choice");
+		roomChoiceLabel = new JLabel();
+		playerChoiceBox = new JComboBox();
+		for(Card nextPlayerCard : board.getPlayerCards()){ // For all player cards
+			playerChoiceBox.addItem(nextPlayerCard.getCardName()); // Add the name of the card to the combo box as an option
+		}
+		weaponChoiceBox = new JComboBox();
+		for(Card nextWeaponCard : board.getWeaponCards()){ // For all weapon cards
+			weaponChoiceBox.addItem(nextWeaponCard.getCardName()); // Add the name of the card to the combo box as an option
+		}
+		roomChoice.add(roomSuggestionLabel);
+		roomChoice.add(roomChoiceLabel);
+		playerChoice.add(playerSuggestionLabel);
+		playerChoice.add(playerChoiceBox);
+		weaponChoice.add(weaponSuggestionLabel);
+		weaponChoice.add(weaponChoiceBox);
+
+		suggestionChoices.add(roomChoice);
+		suggestionChoices.add(playerChoice);
+		suggestionChoices.add(weaponChoice);
+
+		add(suggestionChoices, BorderLayout.CENTER);
+
+	}
+	
+	public void setRoomLabel(String currentRoom){
+		roomChoiceLabel.setText(currentRoom);
+	}
+
+}
